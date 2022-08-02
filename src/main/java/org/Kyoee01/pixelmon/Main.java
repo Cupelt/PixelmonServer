ddee597ff8381bce591f46742dd1ad5c72f9ccaa
Colda4988 <mj59602896@gmail.con>
2022-08-03 06:48:41 +0900

    Initial commit

---
+++ src/main/java/org/Kyoee01/pixelmon/Main.java
@@ -0,0 +1,10 @@
+package org.Kyoee01.pixelmon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Enable Pixelmon Main Plugin");
+    }
+}
