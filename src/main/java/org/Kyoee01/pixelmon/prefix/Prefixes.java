f0c4264239396739afae68d566db9be727a639bf
Colda4988 <mj59602896@gmail.con>
2022-08-05 01:14:05 +0900

    added Error Prefix

--- src/main/java/org/Kyoee01/pixelmon/prefix/Prefixes.java
+++ src/main/java/org/Kyoee01/pixelmon/prefix/Prefixes.java
@@ -3,12 +3,18 @@ package org.Kyoee01.pixelmon.prefix;
 public class Prefixes {
 
     private final String Server;
+    private final String Error;
 
     public Prefixes(){
         this.Server = "&f&l[&6&lSERVER&f&l] &f";
+        this.Error = "&c&l[&4&lERROR&f&l] &4";
     }
 
     public String getServer() {
         return Server;
+    }
+
+    public String getError() {
+        return Error;
     }
 }
