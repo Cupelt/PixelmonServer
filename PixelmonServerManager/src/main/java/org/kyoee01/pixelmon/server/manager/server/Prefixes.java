8d78b4f8554590d680be5176d1e579f1234c5203
Colda4988 <mj59602896@gmail.con>
2022-08-10 07:14:23 +0900

    Fix cannot be pushed on github

---
+++ PixelmonServerManager/src/main/java/org/kyoee01/pixelmon/server/manager/server/Prefixes.java
@@ -0,0 +1,29 @@
+package org.Kyoee01.pixelmon.server.manager.server;
+
+public class Prefixes {
+    private static String Server = "&f&l[&6&lSERVER&f&l] &f";
+    private static String Error = "&c&l[&4&lERROR&c&l] &4";
+    private static String Event = "&f&l[&e&lEVENT&f&l] &f";
+    private static String Join = "&6&l[&a&lJoin&6&l] &f";
+    private static String Quit = "&6&l[&4&lQuit&6&l] &f";
+
+    public static String getServer(){
+        return Server;
+    }
+
+    public static String getError() {
+        return Error;
+    }
+
+    public static String getEvent() {
+        return Event;
+    }
+
+    public static String getJoin() {
+        return Join;
+    }
+
+    public static String getQuit() {
+        return Quit;
+    }
+}
